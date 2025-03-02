6cb01589ff991e9225dcb0467f98f0c5b882599d
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-03 02:11:12 +0300

    redesign projects

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -1,35 +1,32 @@
 'use client'
-
 import { useState } from 'react'
 import ProjectsCard from './ProjectsCard'
-import { Collapsible } from '@radix-ui/react-collapsible'
-import { CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
-import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from '@/components/ui/carousel'
 
 const Projects = () => {
-
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-
-  function handleOpen() {
-    setIsOpen(!isOpen)
-  }
-
+  
   return (
-    <div id='projects' className='mt-16 flex flex-col px-16 '>
-        <h2 className='text-center text-4xl font-bold mb-8'>Наши проекты</h2>
-        <Collapsible className='flex flex-col gap-8 items-center'>
-          <ProjectsCard title='Чат-Бот "Магаз Дядюшки v2"' imageSrc='/uc_shop.png' linkHref='https://t.me/syntex_devlogs/14'/>
-          <ProjectsCard title='Чат-Бот "Халява Дядюшки v2"' imageSrc='/free.png' linkHref='https://t.me/syntex_devlogs/6'/>
-          <ProjectsCard title='Чат-Бот "Халява от Кошмара v2"' imageSrc='/free_koshmar.png' linkHref='https://t.me/syntex_devlogs/12'/>
-          <CollapsibleContent className='flex flex-col gap-8 items-center'>
+    <div id='projects' className='mt-16 flex flex-col px-16'>
+      <h2 className='text-center text-4xl font-bold mb-8'>Наши проекты</h2>
+      
+      {/* Карусель без встроенных кнопок навигации */}
+      <Carousel className='max-w-full'>
+        <CarouselContent>
+          <CarouselItem className='pl-4 basis-1/2'>
             <ProjectsCard title='Чат-Бот "Магаз Дядюшки v2"' imageSrc='/uc_shop.png' linkHref='https://t.me/syntex_devlogs/14'/>
+          </CarouselItem>
+          <CarouselItem className='pl-4 basis-1/2'>
             <ProjectsCard title='Чат-Бот "Халява Дядюшки v2"' imageSrc='/free.png' linkHref='https://t.me/syntex_devlogs/6'/>
+          </CarouselItem>
+          <CarouselItem className='pl-4 basis-1/2'>
             <ProjectsCard title='Чат-Бот "Халява от Кошмара v2"' imageSrc='/free_koshmar.png' linkHref='https://t.me/syntex_devlogs/12'/>
-          </CollapsibleContent>
-          <CollapsibleTrigger asChild>
-            <Button onClick={handleOpen} className=''>Показать {(isOpen ? 'меньше' : 'больше')}</Button>
-          </CollapsibleTrigger>
-        </Collapsible>
+          </CarouselItem>
+        </CarouselContent>
+        <div className="flex justify-center gap-4 mt-6 text-black">
+          <CarouselPrevious className="static translate-y-0"/>
+          <CarouselNext className="static translate-y-0" />
+        </div>
+      </Carousel>
     </div>
   )
 }
--- app/components/projects/ProjectsCard.tsx
+++ app/components/projects/ProjectsCard.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-
 import Image from 'next/image'
 import Link from 'next/link'
-import { ArrowUpRight } from 'lucide-react'
+import { Card, CardContent } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 
 interface ProjectsCardProps {
     title: string
@@ -12,13 +11,16 @@ interface ProjectsCardProps {
 
 const ProjectsCard = ({title, linkHref, imageSrc}: ProjectsCardProps) => {
   return (
-    <div className='flex flex-row gap-8'>
+    <Card className='border border-zinc-800 rounded-xl p-4 bg-black text-white'>
+      <CardContent className='flex flex-row gap-8'>
         <Image src={imageSrc} alt={title} width={384} height={384} className='rounded-xl'/>
         <div className="flex flex-col">
-            <h3 className='text-2xl font-bold mb-2'>{title}</h3>
-            <p >Узнать больше: <Link className='underline' href={linkHref}>{linkHref}</Link></p>
+            <h3 className='text-2xl font-bold flex-grow'>{title}</h3>
+            <Link className='underline' href={linkHref}><Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300'>Узнать больше</Button></Link>
         </div>
-    </div>
+      </CardContent>
+    </Card>
+
   )
 }
 
