98b558f9d1cb2eeb81a880c7e57e813541c9fcc1
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-02 15:44:17 +0300

    partners fix

--- app/components/projects/partners/Partners.tsx
+++ app/components/projects/partners/Partners.tsx
@@ -6,9 +6,8 @@ const Partners = () => {
     <div id='partners' className='mt-16 flex flex-col px-16'>
         <h2 className='text-center text-4xl font-bold mb-8'>Наши партнёры</h2>
         <div className='flex flex-row items-center justify-center grayscale gap-32'>
-            <Image src={'/partners/codeepay.png'} width={128} height={128} alt='Codeepay'/>
-            <Image src={'/partners/xpay.png'} width={128} height={128} alt='XPAY'/>
-            <Image src={'/partners/dyad_andrey.png'} width={128} height={128} alt='dyad_andrey'/>
+            <Image className='rounded-xl' src={'/partners/xpay.png'} width={128} height={128} alt='XPAY'/>
+            <Image className='rounded-xl' src={'/partners/dyad_andrey.png'} width={128} height={128} alt='dyad_andrey'/>
         </div>
     </div>
   )
