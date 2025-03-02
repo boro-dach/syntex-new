a9d923d656931d8eaaea7090082df6be4d9bcb9b
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-02 18:00:50 +0300

    make services collapsable

--- app/components/services/ServicesCard.tsx
+++ app/components/services/ServicesCard.tsx
@@ -1,5 +1,5 @@
-import { IconNode, LucideIcon } from 'lucide-react'
-import React from 'react'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
+import { LucideIcon } from 'lucide-react'
 
 interface ServicesCardProps {
     title: string
@@ -10,12 +10,12 @@ interface ServicesCardProps {
 
 const ServicesCard = ({title, text, number, icon: Icon}: ServicesCardProps) => {
   return (
-    <div className='flex flex-col'>
-      <h3 className='text-6xl font-bold border-b border-zinc-800 pb-4 flex flex-row items-center'><span className='pr-8 text-lg align-top font-bold'>{number}</span>{title} <Icon size={32} className='mt-4 ml-4'/></h3>
-      <div className="grid grid-cols-2 grid-rows-1 mt-4">
+    <Collapsible className='flex flex-col'>
+      <CollapsibleTrigger><h3 className='text-6xl font-bold border-b border-zinc-800 pb-4 flex flex-row items-center'><span className='pr-8 text-lg align-top font-bold'>{number}</span>{title} <Icon size={32} className='mt-4 ml-4'/></h3></CollapsibleTrigger>
+      <CollapsibleContent className="grid grid-cols-2 grid-rows-1 mt-4">
         <p>{text}</p>
-      </div>
-    </div>
+      </CollapsibleContent>
+    </Collapsible>
   )
 }
 
