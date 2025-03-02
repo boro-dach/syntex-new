481f7e8ba082bd36e23c0d1dd1d6e70353176e88
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-03 00:39:28 +0300

    moved partners

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import Services from './components/services/Services'
 import Projects from './components/projects/Projects'
-import Partners from './components/projects/partners/Partners'
+import Partners from './components/partners/Partners'
 
 const Home = () => {
   return (
