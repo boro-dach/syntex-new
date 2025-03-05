53777166094799592a930514e984d443940a9967
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-05 20:29:22 +0100

    projects carousel autoplay

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -1,14 +1,24 @@
 'use client'
-import { useState } from 'react'
 import ProjectsCard from './ProjectsCard'
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from '@/components/ui/carousel'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from '@/components/ui/carousel'
+import Autoplay from "embla-carousel-autoplay"
+import React from 'react'
 
 const Projects = () => {
+
+  const plugin = React.useRef(
+    Autoplay({ delay: 5000, stopOnInteraction: true })
+  )
   
   return (
     <div id='projects' className='mt-16 flex flex-col px-16'>
       <h2 className='text-center text-4xl font-bold mb-8'>Наши проекты</h2>
-      <Carousel className='max-w-full'>
+      <Carousel 
+        className='max-w-full'
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={() => {plugin.current.play()}}
+      >
         <CarouselContent>
           <CarouselItem className='pl-4 basis-1/2'>
             <ProjectsCard title='Чат-Бот "Магаз Дядюшки v2"' imageSrc='/uc_shop.png' linkHref='https://t.me/syntex_devlogs/14'/>
