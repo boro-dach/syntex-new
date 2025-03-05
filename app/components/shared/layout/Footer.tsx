9572fc3692057dfcc822a94b352d098d9d1c1b5c
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-05 22:13:28 +0100

    footer design update

--- app/components/shared/layout/Footer.tsx
+++ app/components/shared/layout/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div id='footer' className='mt-16 flex flex-row items-center px-16 gap-8 h-16 border-t border-zinc-400 py-16'>
-        <h2 className='text-center text-4xl font-bold'>Контакты</h2>
-        <Link className='underline' href={'https://t.me/zako_abb'}>https://t.me/zako_abb</Link>
-        <Link type='mail' className='underline' href={'mailto:ceo@syntex-dev.ru'}>ceo@syntex-dev.ru</Link>
+    <div id='footer' className='my-8 flex flex-col px-16 border-t border-zinc-400'>
+        <h2 className='text-5xl font-bold my-8'>Связаться с нами:</h2>
+        <Link className='underline mb-4 w-fit text-xl' href={'https://t.me/zako_abb'}>https://t.me/zako_abb</Link>
+        <Link type='mail' className='underline w-fit text-xl' href={'mailto:ceo@syntex-dev.ru'}>ceo@syntex-dev.ru</Link>
     </div>
   )
 }
