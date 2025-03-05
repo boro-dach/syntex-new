7c023a312b660f749da93831b9d4697c17610379
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-05 21:55:47 +0100

    patners animation

--- app/components/partners/Partners.tsx
+++ app/components/partners/Partners.tsx
@@ -1,16 +1,50 @@
-import Image from 'next/image'
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import { motion } from 'framer-motion'; // Импортируем motion
+import { useInView } from 'react-intersection-observer'; // Импортируем useInView
 
 const Partners = () => {
-  return (
-    <div id='partners' className='mt-16 flex flex-col px-16'>
-        <h2 className='text-center text-4xl font-bold mb-8'>Наши партнёры</h2>
-        <div className='flex flex-row items-center justify-center grayscale gap-32'>
-            <Image className='rounded-xl' src={'/partners/xpay.png'} width={128} height={128} alt='XPAY'/>
-            <Image className='rounded-xl' src={'/partners/dyad_andrey.png'} width={128} height={128} alt='dyad_andrey'/>
-        </div>
-    </div>
-  )
-}
+  // Используем useInView для отслеживания видимости компонента
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Анимация сработает только один раз
+    threshold: 0.1, // Порог видимости (10%)
+  });
 
-export default Partners
+  // Варианты анимации
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+  };
+
+  return (
+    <motion.div
+      id='partners'
+      className='mt-16 flex flex-col px-16'
+      ref={ref} // Привязываем ref к компоненту
+      initial="hidden" // Начальное состояние
+      animate={inView ? 'visible' : 'hidden'} // Анимация при появлении
+      variants={variants} // Варианты анимации
+    >
+      <h2 className='text-center text-4xl font-bold mb-8'>Наши партнёры</h2>
+      <div className='flex flex-row items-center justify-center grayscale gap-32'>
+        <Image
+          className='rounded-xl'
+          src={'/partners/xpay.png'}
+          width={128}
+          height={128}
+          alt='XPAY'
+        />
+        <Image
+          className='rounded-xl'
+          src={'/partners/dyad_andrey.png'}
+          width={128}
+          height={128}
+          alt='dyad_andrey'
+        />
+      </div>
+    </motion.div>
+  );
+};
+
+export default Partners;
