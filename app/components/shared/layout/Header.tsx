c47dfd9002d19c8315f478bd71b500a34d22d04c
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-07 17:31:36 +0100

    add header burger menu

--- app/components/shared/layout/Header.tsx
+++ app/components/shared/layout/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Menu } from 'lucide-react'
 
 const Header = () => {
   return (
@@ -12,7 +14,21 @@ const Header = () => {
             <Link href='#projects'>Проекты</Link>
             <Link href='#partners'>Партнёры</Link>
         </div>
-        <Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300 font-bold'>Создать проект</Button>
+        <div className="flex flex-row items-center gap-2">
+          <Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300 font-bold'>Создать проект</Button>
+            <div className="md:hidden block">
+              <DropdownMenu>
+                <DropdownMenuTrigger className='border border-zinc-800 rounded-md p-2'>
+                  <Menu/>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className='bg-black text-white border-zinc-800'>
+                  <DropdownMenuItem className='bg-black'><Link href='#services'>Услуги</Link></DropdownMenuItem>
+                  <DropdownMenuItem className='bg-black'><Link href='#projects'>Проекты</Link></DropdownMenuItem>
+                  <DropdownMenuItem className='bg-black'><Link href='#partners'>Партнёры</Link></DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+        </div>
     </div>
   )
 }
