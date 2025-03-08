adf94a11cc7af70ea19f216d426669df8f974447
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-08 21:30:26 +0100

    bigger margins for mobiles

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -24,7 +24,7 @@ const Projects = () => {
   return (
     <motion.div
       id='projects'
-      className='mt-16 flex flex-col md:px-16 px-8'
+      className='md:mt-16 mt-24 flex flex-col md:px-16 px-8'
       ref={ref} 
       initial="hidden" 
       animate={inView ? 'visible' : 'hidden'} 
