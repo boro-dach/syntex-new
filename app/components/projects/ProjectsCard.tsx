a313fb3f204c9b3d95ee8aeb9d3c8bf4c72dbe5d
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-08 21:16:56 +0100

    improvements to projects carousel adaptation

--- app/components/projects/ProjectsCard.tsx
+++ app/components/projects/ProjectsCard.tsx
@@ -10,12 +10,13 @@ interface ProjectsCardProps {
 
 const ProjectsCard = ({title, linkHref, imageSrc}: ProjectsCardProps) => {
   return (
-    <Card className='border border-zinc-800 rounded-xl py-4 bg-black text-white h-full max-w-full'>
+    <Card className='border border-zinc-800 rounded-xl pt-4 mx-2 bg-black text-white h-full max-w-full'>
       <CardContent className='flex flex-row md:gap-8 gap-4 h-full'>
         <img src={imageSrc} alt={title} className='rounded-xl md:h-96 md:w-96 h-32 w-32 aspect-square'/>
         <div className="flex flex-col">
             <h3 className='md:text-2xl text-lg md:max-w-full max-w-32 break-words font-bold flex-grow'>{title}</h3>
-            <Link className='inline-block w-fit' href={linkHref}><Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300 md:text-base text-sm'>Узнать больше</Button></Link>
+            <Link className='md:inline-block hidden w-fit' href={linkHref}><Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300 md:text-base text-sm'>Узнать больше</Button></Link>
+            <Link className='md:hidden inline-block w-fit' href={linkHref}><Button className='bg-white text-black hover:bg-zinc-200 transition-all duration-300 md:text-base text-sm'>Больше</Button></Link>
         </div>
       </CardContent>
     </Card>
