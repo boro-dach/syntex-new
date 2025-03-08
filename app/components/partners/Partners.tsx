71aee71074b8502385efa1d5b63b0315b4f43d47
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-08 21:59:03 +0100

    add links to partners

--- app/components/partners/Partners.tsx
+++ app/components/partners/Partners.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import React from 'react';
 import { motion } from 'framer-motion'; 
 import { useInView } from 'react-intersection-observer';
+import Link from 'next/link';
 
 const Partners = () => {
 
@@ -27,16 +28,20 @@ const Partners = () => {
     >
       <h2 className='text-center text-4xl font-bold mb-8'>НАШИ ПАРТНЁРЫ</h2>
       <div className='flex flex-row items-center justify-center grayscale md:gap-32 gap-8'>
-        <img
-          className='rounded-xl md:w-32 md:h-32 w-24 h-24'
-          src={'/partners/ezepay.png'}
-          alt='EzePay'
-        />
-        <img
-          className='rounded-xl md:w-32 md:h-32 w-24 h-24'
-          src={'/partners/dyad_andrey.png'}
-          alt='dyad_andrey'
-        />
+        <Link href='https://ezepay.org/'>
+          <img
+            className='rounded-xl md:w-32 md:h-32 w-24 h-24'
+            src={'/partners/ezepay.png'}
+            alt='EzePay'
+          />
+        </Link>
+        <Link href='https://t.me/BAZADYADKI'>
+          <img
+            className='rounded-xl md:w-32 md:h-32 w-24 h-24'
+            src={'/partners/dyad_andrey.png'}
+            alt='dyad_andrey'
+          />
+        </Link>
       </div>
     </motion.div>
   );
