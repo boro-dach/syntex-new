1f4ae30df0abece1fe95d27931eb4d9b09da8d2f
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-03-08 21:09:00 +0100

    footer links to buttons

--- app/components/shared/layout/Footer.tsx
+++ app/components/shared/layout/Footer.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import React from 'react';
 import { motion } from 'framer-motion'; // Импортируем motion
 import { useInView } from 'react-intersection-observer'; // Импортируем useInView
+import { Button } from '@/components/ui/button';
+import { Mail, Send } from 'lucide-react';
 
 const Footer = () => {
   // Используем useInView для отслеживания видимости компонента
@@ -27,12 +29,15 @@ const Footer = () => {
       variants={variants} 
     >
       <h2 className='md:text-5xl text-2xl font-bold mt-8 md:mb-8 mb-4'>СВЯЗАТЬСЯ С НАМИ:</h2>
-      <Link className='underline mb-4 w-fit md:text-xl text-lg' href={'https://t.me/zako_abb'}>
-        https://t.me/zako_abb
-      </Link>
-      <Link type='mail' className='underline w-fit md:text-xl text-lg' href={'mailto:ceo@syntex-dev.ru'}>
-        ceo@syntex-dev.ru
-      </Link>
+      <div className="flex flex-row items-center md:gap-8 gap-4">
+        <Link className='w-fit rounded-full' href={'https://t.me/zako_abb'}>
+          <Button className='rounded-full md:w-16 md:h-16 w-8 h-8 md:p-4 p-0 bg-white hover:bg-zinc-400 text-black'><img className='md:w-8 md:h-8 w-4 h-4' src="/svgs/telegram.svg" alt=""/></Button>
+        </Link>
+        <Link type='mail' className='md:text-xl text-lg' href={'mailto:ceo@syntex-dev.ru'}>
+          <Button className='rounded-full md:w-16 md:h-16 w-8 h-8 md:p-4 p-0 bg-white hover:bg-zinc-400 text-black'><img className='md:w-8 md:h-8 w-4 h-4' src="/svgs/mail.svg" alt="" /></Button>
+        </Link>
+      </div>
+
     </motion.div>
   );
 };
